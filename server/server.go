9d50e15efff60b243ea0ceb383ba496e5cc8b26e
Iñaki García <yarimfuego@gmail.com>
2022-06-17 21:23:32 -0500

    Websockets added

--- server/server.go
+++ server/server.go
@@ -7,8 +7,10 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/rs/cors"
 	"platzi.com/go/rest-ws/database"
 	"platzi.com/go/rest-ws/repository"
+	"platzi.com/go/rest-ws/websocket"
 )
 
 type Config struct {
@@ -19,16 +21,22 @@ type Config struct {
 
 type Server interface {
 	Config() *Config // Para que sea considerado un servidor deberá tener un Config que retorne un elemento de la clase Config
+	Hub() *websocket.Hub
 }
 
 type Broker struct {
 	config *Config
 	router *mux.Router
+	hub    *websocket.Hub
 }
 
 // Suscribimos el Broker al Server
 func (b *Broker) Config() *Config {
 	return b.config
+}
+
+func (b *Broker) Hub() *websocket.Hub {
+	return b.hub
 }
 
 // Constructor
@@ -46,24 +54,29 @@ func NewServer(ctx context.Context, config *Config) (*Broker, error) {
 	broker := &Broker{
 		config: config,
 		router: mux.NewRouter(), // Define una nueva instancia del router
+		hub:    websocket.NewHub(),
 	}
 
 	return broker, nil
 }
 
 func (b *Broker) Start(binder func(s Server, r *mux.Router)) {
-	b.router = mux.NewRouter() // Insrancia de un router
+	b.router = mux.NewRouter() // Instancia de un router
 	binder(b, b.router)
+	handler := cors.Default().Handler(b.router) // o cors.AllowAll().Handler(b.router)
 
 	repo, err := database.NewPostgresRepository(b.config.DatabaseUrl)
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	go b.hub.Run()
+
 	repository.SetRepository(repo)
 
 	log.Println("Starting server on port", b.Config().Port)
 
-	if err := http.ListenAndServe(b.config.Port, b.router); err != nil {
+	if err := http.ListenAndServe(b.config.Port, handler); err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
 }
